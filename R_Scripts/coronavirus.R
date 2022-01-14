094d075a52d702f76e465b64a26a776858a7234e
MFatihTuzen <49712324+MFatihTuzen@users.noreply.github.com>
2022-01-14 21:56:02 +0300

    Add files via upload

---
+++ R_Scripts/coronavirus.R
@@ -0,0 +1,25 @@
+devtools::install_github("RamiKrispin/coronavirus")
+
+# coronavirus verisi
+library(coronavirus)
+data(coronavirus)
+head(coronavirus)
+
+nrow(coronavirus)
+summary(coronavirus)
+
+
+coronavirus %>%
+  filter(type == "confirmed") %>%
+  group_by(date) %>%
+  summarise(cases = sum(cases)) %>%
+  arrange(desc(date))
+
+library(ggplot2)
+coronavirus %>%
+  filter(type != "recovery") %>%
+  group_by(date,type) %>%
+  summarise(cases = sum(cases)) %>%
+  ggplot(aes(x=date,y=cases)) +
+  geom_col(col="blue") +
+  facet_wrap(~type,scales = "free",nrow = 2)
